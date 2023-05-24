39bc44e8c8ada451ca4843aeb82b56f1786a1256
Melina Luetkehoelter <mlluetke@ncsu.edu>
2023-05-23 22:28:17 -0400

    updates to sql alchemy uri

--- apps/__init__.py
+++ apps/__init__.py
@@ -37,7 +37,7 @@ def configure_database(app):
 
             # fallback to SQLite
             basedir = os.path.abspath(os.path.dirname(__file__))
-            app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db.sqlite3')
+            app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 
             print('> Fallback to SQLite ')
             db.create_all()
