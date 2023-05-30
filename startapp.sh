a56e43012e8a70243dd5748bbd8a2e3e2e76acba
Melina Luetkehoelter <mlluetke@ncsu.edu>
2023-05-30 09:15:37 -0400

    update startapp with new env variables
    update db variables

--- startapp.sh
+++ startapp.sh
@@ -7,12 +7,13 @@ export DB_HOST=192.168.0.179
 export DB_PORT='5433'
 export DB_NAME=acrp     
 #export SQLALCHEMY_DATABASE_URI='postgresql://mlf:rainbows@192.168.0.179:5433/acrp'
-export RENDER_DB_URL='postgresql://mfoust:zChN3MF18PFWdAoN4vmrclQSZWljgvZh@dpg-chn16v67avj3o34jnabg-a.ohio-postgres.render.com/acrp'
-export RENDER_DB_HOST='dpg-chn16v67avj3o34jnabg-a.ohio-postgres.render.com'
+export SQLALCHEMY_DATABASE_URI='postgres://mfoust:XyjzYX9Acng2dTwg4rIWFUA2Q6Osm2w5@dpg-chqva9fdvk4goev0hcv0-a/acrp_yq3c'
+export RENDER_DB_URL='postgresql://mfoust:XyjzYX9Acng2dTwg4rIWFUA2Q6Osm2w5@dpg-chqva9fdvk4goev0hcv0-a.frankfurt-postgres.render.com/acrp_yq3c'
+export RENDER_DB_HOST='dpg-chqva9fdvk4goev0hcv0-a'
 export RENDER_DB_PORT='5432'
-export RENDER_DB_NAME=acrp
+export RENDER_DB_NAME=acrp_yq3c
 export RENDER_DB_USERNAME=mfoust
-export RENDER_DB_PASSWORD=zChN3MF18PFWdAoN4vmrclQSZWljgvZh
+export RENDER_DB_PASSWORD=XyjzYX9Acng2dTwg4rIWFUA2Q6Osm2w5
 export YOUR_API_KEY=AIzaSyDDRJijGYKeAfDEUywTOM3AfvHx1Tc_Eho
 export YOUR_SEARCH_ENGINE_ID_ACRP='13b46c106c4834523'
 flask run #--debugger
